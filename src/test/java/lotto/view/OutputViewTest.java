4f124563f345dabb67627284141197bb707b33e6
Damsul <gyoung9707@naver.com>
2022-11-16 00:15:31 +0900

    test: 테스트 코드 수정

--- src/test/java/lotto/view/OutputViewTest.java
+++ src/test/java/lotto/view/OutputViewTest.java
@@ -2,6 +2,7 @@ package lotto.view;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import lotto.domain.LottoTickets;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
